de7003862a8eb8d552e8dc0cd44a90454949d100
gzhan0226 <gzhan@naver.com>
2024-11-21 02:47:21 +0900

    Initial commit

---
+++ src/main/java/com/example/festOn/FestOnApplication.java
@@ -0,0 +1,13 @@
+package com.example.festOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/festOn/FestOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.festOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
