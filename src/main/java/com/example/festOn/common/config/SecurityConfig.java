e8651f04e88e85020329f2448182dc480bc2334f
baeksom <97323941+baeksom@users.noreply.github.com>
2024-11-23 03:23:10 +0900

    env: 공통 환경 설정 추가

--- src/main/java/com/example/festOn/common/config/SecurityConfig.java
+++ src/main/java/com/example/festOn/common/config/SecurityConfig.java
@@ -1,2 +1,127 @@
-package com.example.festOn.common.config;public class SecurityConfig {
+package com.example.festOn.common.config;
+
+import com.example.festOn.application.user.service.UserService;
+import com.example.festOn.common.auth.filter.JwtAuthenticationFilter;
+import com.example.festOn.common.auth.jwt.JwtTokenProvider;
+import com.example.festOn.common.auth.service.RefreshTokenService;
+import com.example.festOn.common.oauth2.handler.CustomAuthenticationSuccessHander;
+import com.example.festOn.common.oauth2.service.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CorsProperties corsProperties;
+    private final UrlProperties urlProperties;
+
+    private static final String[] ALLOWED_URL = {
+            "/",
+            "/index.html",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/api-docs",
+            "/api-docs/**",
+            "/api/token/reissue",
+            "/oauth/**",
+            "/oauth2/**",
+            "/oauth/login/kakao",
+            "/login-failure",
+            "/error"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .csrf(CsrfConfigurer::disable)
+                .headers(httpSecurityHeadersConfigurer ->
+                        httpSecurityHeadersConfigurer.frameOptions(
+                                HeadersConfigurer.FrameOptionsConfig::disable)
+                        )
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(ALLOWED_URL).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/oauth2/authorization/kakao")
+                        .authorizationEndpoint(authorization -> authorization
+                                .baseUri("/oauth2/authorization")
+                        )
+                        .tokenEndpoint(token -> token
+                                .accessTokenResponseClient(kakaoAccessTokenResponseClient())
+                        )
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserService())
+                        )
+                        .successHandler(customAuthenticationSuccessHander())
+                        .failureHandler(new SimpleUrlAuthenticationFailureHandler("/login-failure"))
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: Access token is invalid or expired");
+                        })
+                );
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(corsProperties.getAllowedOrigins());
+        configuration.setAllowedMethods(corsProperties.getAllowedMethods());
+        configuration.setAllowedHeaders(corsProperties.getAllowedHeaders());
+        configuration.setAllowCredentials(corsProperties.getAllowCredentials());
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        return new CustomOAuth2UserService(userService);
+    }
+
+    @Bean
+    public CustomAuthenticationSuccessHander customAuthenticationSuccessHander() {
+        return new CustomAuthenticationSuccessHander(jwtTokenProvider, refreshTokenService, urlProperties);
+    }
+
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> kakaoAccessTokenResponseClient() {
+        return new DefaultAuthorizationCodeTokenResponseClient();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtTokenProvider);
+    }
 }
--- src/main/java/com/example/festOn/common/config/UrlProperties.java
+++ src/main/java/com/example/festOn/common/config/UrlProperties.java
@@ -1,2 +1,21 @@
-package com.example.festOn.common.config;public class UrlProperties {
+package com.example.festOn.common.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+public class UrlProperties {
+
+    @Value("${spring.url.redirectUrl.signUp}")
+    private String signUpUrl;
+
+    @Value("${spring.url.redirectUrl.main}")
+    private String mainUrl;
+
+    @Value("${spring.url.domain}")
+    private String domain;
 }
